2d9883ecd649c78fec5acfd6bd73d6c063921e81
jonatas <jonatas.dias@estuante.ufjf.br>
2023-08-16 13:18:51 -0300

    updating py archives

--- python/main.py
+++ python/main.py
@@ -4,5 +4,14 @@ import subprocess
 
 #command = ['python', 'runFiles.py', 've-mech']
 
-command = ['python', 'runFiles.py', 've-poisson']
+#command = ['python', 'runFiles.py', 'poisson-endo0-f1']
+
+#subprocess.run(command)
+
+command = ['python', 'runFiles.py', 've-mech-inc2']
+
+subprocess.run(command)
+
+command = ['python', 'runFiles.py', 've-mech-inc10']
+
 subprocess.run(command)
